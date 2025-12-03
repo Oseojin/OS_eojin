03f70ef97c3a5c9efef695e378ce99c2154dfc0f
Oseojin <oseojin5137@gmail.com>
2025-12-04 00:16:53 +0900

    change 64 bits idt.h

--- includes/idt.h
+++ includes/idt.h
@@ -8,16 +8,18 @@ typedef struct
 {
     uint16_t    low_offset;     // 핸들러 주소 하위 16비트
     uint16_t    sel;            // 커널 코드 세그먼트 셀렉터
-    uint8_t     always0;        // 항상 0
+    uint8_t     ist;            // 인터럽트 스택 테이블 (0)
     uint8_t     flags;          // 타입 및 속성 (P, DPL, Type)
-    uint16_t    high_offset;    // 핸들러 주소 상위 16비트
+    uint16_t    mid_offset;     // 16-31 비트
+    uint16_t    high_offset;    // 32-63 비트
+    uint32_t    reserved;       // 0
 } __attribute__((packed))   idt_gate_t;
 
 // IDT 포인터 구조체 (lidt 명령어용)
 typedef struct
 {
     uint16_t    limit;
-    uint32_t    base;
+    uint64_t    base;
 } __attribute__((packed))   idt_register_t;
 
 // 총 256개 인터럽트 게이트
