134ab3cd91beb00b6b6d6a05570d9788f56b2ad1
Oseojin <oseojin5137@gmail.com>
2025-12-04 00:19:48 +0900

    change 64-bits idt.c

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -3,18 +3,20 @@
 idt_gate_t      idt[IDT_ENTRIES];
 idt_register_t  idt_reg;
 
-void    set_idt_gate(int n, uint32_t handler)
+void    set_idt_gate(int n, uint64_t handler)
 {
-    idt[n].low_offset = (uint16_t)((handler) & 0xffff);
-    idt[n].sel = 0x08;  // GDT의 커널 코드 세그먼트 오프셋
-    idt[n].always0 = 0;
-    idt[n].flags = 0x8e; // 1(P) 00(DPL) 0 1110(32-bit Interrupt Gate)
-    idt[n].high_offset = (uint16_t)(((handler) >> 16) & 0xffff);
+    idt[n].low_offset = (uint16_t)handler;
+    idt[n].sel = 0x08;
+    idt[n].ist = 0;
+    idt[n].flags = 0x8e;
+    idt[n].mid_offset = (uint16_t)(handler >> 16);
+    idt[n].high_offset = (uint32_t)(handler >> 32);
+    idt[n].reserved = 0;
 }
 
 void    set_idt()
 {
-    idt_reg.base = (uint32_t)&idt;
+    idt_reg.base = (uint64_t)&idt;
     idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
     // 어셈블리 명령어로 IDT 로드
     __asm__ volatile("lidt (%0)" : : "r" (&idt_reg));
